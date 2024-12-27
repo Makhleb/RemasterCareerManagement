a0be7573dc7972c271a0b4bd21dc84447f224fc4
cgh <cgh9694@gmail.com>
2024-12-27 14:03:01 +0900

    이동 전 저장

---
+++ src/main/java/com/example/test/controller/view/gihwan/JobPostController.java
@@ -0,0 +1,17 @@
+package com.example.test.controller.view.gihwan;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created on 2024-12-27 by 최기환
+ */
+@Controller
+@RequestMapping("/mypage/company")
+public class JobPostController {
+    @GetMapping("/job-post")
+    public String jobPost() {
+        return "/gihwan/job-post";
+    }
+}
