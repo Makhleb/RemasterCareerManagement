cae19eb8736f7a9718bdd16c337fcc42c65fcd5f
sanginjjang <hansam8456@gmail.com>
2024-12-27 12:12:08 +0900

    1227 12:11 header_hsi -> header_hsi

---
+++ src/main/java/com/example/test/controller/view/sangin/InViewController.java
@@ -0,0 +1,15 @@
+package com.example.test.controller.view.sangin;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created on 2024-12-27 by 한상인
+ */
+@RequestMapping("/sangin")
+public class InViewController {
+    @GetMapping("test")
+    public String test() {
+        return "sangin/test";
+    }
+}
