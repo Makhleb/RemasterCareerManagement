94d1eab10e217aa8add04ff83072d1534c3f0d2b
cgh <cgh9694@gmail.com>
2024-12-27 09:58:00 +0900

    기존 설정 추가 완료

--- src/main/java/com/example/test/config/SecurityConfig.java
+++ src/main/java/com/example/test/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.test.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -24,6 +25,10 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((auth) -> auth
                         // todo 회원가입/로그인 완료되면 퍼밋 수정하기!!!
+                        // todo 아래 주석은 특정 메서드가 작동하지 않을시 사용
+//                        .requestMatchers(HttpMethod.POST, "/api/**").permitAll()
+//                        .requestMatchers(HttpMethod.PUT, "/api/**").hasAnyRole("MEMBER", "ADMIN")
+//                        .requestMatchers(HttpMethod.DELETE, "/api/**").hasAnyRole("ADMIN")
                         .anyRequest().permitAll()
                 );
         return http.build();
