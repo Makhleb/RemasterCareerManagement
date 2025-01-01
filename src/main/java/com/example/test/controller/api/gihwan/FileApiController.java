dc0c73f99ce1093e77ef3eaf6a40eab64a7c0db1
choi-gi-hwan <cgh9694@gmail.com>
2025-01-01 22:29:35 +0900

    파일 서비스 분리

--- src/main/java/com/example/test/controller/api/gihwan/FileApiController.java
+++ src/main/java/com/example/test/controller/api/gihwan/FileApiController.java
@@ -23,10 +23,7 @@ import java.util.*;
 @RequestMapping("/api/common/file")
 public class FileApiController {
 
-    @Value("${file.upload.path}")
-    private String uploadPath;
     private final FileService fileService;
-
 
     private final FileDao fileDao;
 
@@ -35,63 +32,19 @@ public class FileApiController {
         this.fileService = fileService;
     }
 
-
+    /**
+     * 인포 저장 컨트롤러
+     */
     @PostMapping("/file-info")
     public boolean registFileInfo(@RequestBody FileDto fileDto) {
         return fileDao.insertFile(fileDto) != 0;
     }
 
+    /**
+     * 파일 저장
+     */
     @PostMapping
     public boolean registFile(@RequestPart("file") MultipartFile file, @RequestPart FileDto fileDto) {
-        //todo 서비스로 뺍시다. 그리고 이미 파일에 내용이 있다면 삭제? 추가?
-        try {
-            String uploadDir = System.getProperty("user.dir") + "/uploads" + "/" + fileDto.getFileGubn() + "/" + fileDto.getFileRefId();
-            File directory = new File(uploadDir);
-
-            //디렉토리 없으면 추가
-            if (!directory.exists()) {
-                boolean isCreated = directory.mkdirs();  // 디렉토리 생성 여부 확인
-                if (isCreated) {
-                    System.out.println("업로드 디렉터리가 생성되었습니다: " + uploadDir);
-                } else {
-                    throw new IOException("디렉터리 생성 실패");
-                }
-            }
-
-            //uploads에 추가
-            String filePath = uploadDir + File.separator + file.getOriginalFilename();
-            file.transferTo(new File(filePath));
-
-            //파일 업로드 후 db에 정보 등록
-            int result = fileDao.insertFile(fileDto);
-            return result != 0;
-        } catch (IOException e) {
-            return false;
-        }
-    }
-
-    @PostMapping("/list")
-    public Map<String, String> loadImage(@RequestBody List<FileDto> fileDtos) throws IOException {
-        Map<String, String> files = new HashMap<>();
-
-        for (FileDto fileDto : fileDtos) {
-            Path dirPath = Paths.get(uploadPath)
-                    .resolve(fileDto.getFileGubn())
-                    .resolve(fileDto.getFileRefId())
-                    .normalize();
-
-            try (DirectoryStream<Path> stream = Files.newDirectoryStream(dirPath, "*")) {
-                Path filePath = stream.iterator().next();
-                byte[] fileBytes = Files.readAllBytes(filePath);
-                String encodedFile = Base64.getEncoder().encodeToString(fileBytes);
-                files.put(fileDto.getFileRefId(), encodedFile);
-            } catch (NoSuchElementException e) {
-                throw new FileNotFoundException("(오류)파일을 찾을 수 없음:" + dirPath);
-            } catch (Exception e) {
-                throw new IOException("(오류)디렉토리 내 파일 읽기중 오류 발생: " + dirPath, e);
-            }
-        }
-
-        return files;
+        return fileService.saveImage(file, fileDto);
     }
 }
--- src/main/java/com/example/test/service/common/FileService.java
+++ src/main/java/com/example/test/service/common/FileService.java
@@ -1,9 +1,14 @@
 package com.example.test.service.common;
 
+import com.example.test.dao.gihwan.FileDao;
+import com.example.test.dto.FileDto;
 import com.example.test.dto.response.FileResponseDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -16,29 +21,55 @@ import java.util.Base64;
 @Service
 public class FileService {
 
+    private final FileDao fileDao;
     @Value("${file.upload.path}")
     private String uploadPath;
 
-
+    public FileService(FileDao fileDao) {
+        this.fileDao = fileDao;
+    }
 
     public String loadImage(String fileGubn, String refId) {
-
-        FileResponseDto fileResponseDto = new FileResponseDto();
 
         Path dirPath = Paths.get(uploadPath)
                 .resolve(fileGubn)
                 .resolve(refId)
                 .normalize();
-
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(dirPath, "*")) {
             Path filePath = stream.iterator().next();
             byte[] fileBytes = Files.readAllBytes(filePath);
-            String encodedFile = Base64.getEncoder().encodeToString(fileBytes);
 
-            return encodedFile;
+            return Base64.getEncoder().encodeToString(fileBytes);
 
         } catch (Exception e) {
             return null;
+        }
+    }
+
+    public boolean saveImage(MultipartFile file, FileDto fileDto) {
+        try {
+            String uploadDir = System.getProperty("user.dir") + "/uploads" + "/" + fileDto.getFileGubn() + "/" + fileDto.getFileRefId();
+            File directory = new File(uploadDir);
+
+            //디렉토리 없으면 추가
+            if (!directory.exists()) {
+                boolean isCreated = directory.mkdirs();  // 디렉토리 생성 여부 확인
+                if (isCreated) {
+                    System.out.println("업로드 디렉터리가 생성되었습니다: " + uploadDir);
+                } else {
+                    throw new IOException("디렉터리 생성 실패");
+                }
+            }
+
+            //uploads에 추가
+            String filePath = uploadDir + File.separator + file.getOriginalFilename();
+            file.transferTo(new File(filePath));
+
+            //파일 업로드 후 db에 정보 등록
+            int result = fileDao.insertFile(fileDto);
+            return result != 0;
+        } catch (IOException e) {
+            return false;
         }
     }
 }
